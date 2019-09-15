10252daa2b961cc251ca11ea7abb98a03ed11e6a
unknown <seifeldin.alaa@gmail.com>
2019-09-15 21:50:17 +0200

    refactoring

--- src/app_component/form.component.jsx
+++ src/app_component/form.component.jsx
@@ -3,16 +3,16 @@ import "./form.style.css";
 const Form = props => {
     return (
         <div className="container h-100">
-        <form onSubmit={props.loadweather}>
-        <div>{props.error ? error() : ""}</div>
+            <form onSubmit={props.loadweather}>
+                <div>{props.error ? error() : ""}</div>
                 <div className="row">
                     <div className="col-md-3 offset-md-2">
                         <input
-                         type="text" className="form-control" name="city" autoComplete="off" placeholder="City" />
+                            type="text" className="form-control" name="city" autoComplete="off" placeholder="City" />
                     </div>
                     <div className="col-md-3">
                         <input type="text" className="form-control" name="country" autoComplete="off" placeholder="Country" />
-                        </div>
+                    </div>
                     <div className="col-md-3 mt-md-0 m1-2 text-md-left">
                         <button className="btn btn-warning">Get Weather</button>
                     </div>
@@ -25,11 +25,11 @@ const Form = props => {
 
 const error = props => {
     return (
-      <div className="alert alert-danger mx-5" role="alert">
-        Please Enter City and Country
+        <div className="alert alert-danger mx-5" role="alert">
+            Please Enter City and Country
       </div>
     );
-  };
+};
 
 
 export default Form;
